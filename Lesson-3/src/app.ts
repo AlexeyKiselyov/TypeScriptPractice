c6ecb399f0fc368cbbc579bc67c3cfe8f753a3a0
AlexeyKiselyov <aleshakiselev123@gmail.com>
2023-04-05 23:38:31 +0300

    resolved lesson 3

---
+++ Lesson-3/src/app.ts
@@ -0,0 +1,7 @@
+const button = document.querySelector(".button") as HTMLButtonElement;
+
+button.addEventListener("click", () => {
+  console.log("Click");
+  console.log("Next step1");
+  console.log("Next step2");
+});
