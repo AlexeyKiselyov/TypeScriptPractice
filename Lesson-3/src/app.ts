2e85b83eeb70d4e689908ff8578d370454502ce1
AlexeyKiselyov <aleshakiselev123@gmail.com>
2023-04-09 23:16:24 +0300

    TS class practic

--- Lesson-3/src/app.ts
+++ Lesson-3/src/app.ts
@@ -5,3 +5,18 @@ button.addEventListener("click", () => {
   console.log("Next step1");
   console.log("Next step2");
 });
+
+class Device {
+  private move = true;
+  public some = "some";
+
+  public action() {
+    this.move ? console.log("Go") : "stop";
+  }
+}
+
+const Desctop = new Device();
+Desctop.action();
+console.log(Desctop.some);
+Desctop.some = 'any';
+console.log(Desctop.some);
