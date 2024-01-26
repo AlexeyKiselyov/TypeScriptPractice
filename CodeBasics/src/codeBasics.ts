07ea0c4be8216e3d22ede4df50f5e37f6b60117d
AlexeyKiselyov <aleshakiselev123@gmail.com>
2024-01-26 11:52:54 +0200

    TS training

--- CodeBasics/src/codeBasics.ts
+++ CodeBasics/src/codeBasics.ts
@@ -1007,3 +1007,75 @@ console.log(response[0].user); // user1
 console.log(response[0] instanceof UserResponse1); // true
 
 // --------->40 tasks ---> Abstract Class
+
+// Когда нам нужно определить общее поведение для нескольких классов, удобно использовать
+// Абстрактные классы хоть и не могут быть созданы напрямую, однако они могут быть наследованы. Еще они могут указать явно, какой метод должен быть реализован в наследниках:
+
+abstract class CustomFile7 {
+  protected name: string;
+
+  protected size: number;
+
+  constructor(name: string, size: number) {
+    this.name = name;
+    this.size = size;
+  }
+
+  sizeInKb(): number {
+    return this.size / 1024;
+  }
+}
+
+class ImageCustomFile7 extends CustomFile7 {
+  constructor(name: string, size: number) {
+    super(name, size);
+  }
+}
+
+// Чтобы выносить из классов общую часть кода, абстрактные классы активно используются для построения архитектуры приложения и фреймворков. Например, в React есть класс Component, который может быть представлен как абстрактный класс. Мы не можем создать его напрямую, но он требует от наследников реализации метода render. Это позволяет создавать компоненты, которые будут рендериться при инициализации:
+
+abstract class Component {
+  abstract render(): void;
+
+  constructor() {
+    this.render();
+  }
+}
+
+// exercise
+
+abstract class Clock {
+  constructor(
+    protected hours: number,
+    protected minutes: number,
+    protected seconds: number
+  ) {
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.render();
+  }
+
+  tick(): void {
+    this.seconds += 1;
+    if (this.seconds === 60) {
+      this.seconds = 0;
+      this.minutes += 1;
+    }
+    if (this.minutes === 60) {
+      this.minutes = 0;
+      this.hours += 1;
+    }
+    if (this.hours === 24) {
+      this.hours = 0;
+      this.minutes = 0;
+      this.seconds = 0;
+    }
+  }
+
+  abstract render(): string;
+}
+
+// ---------> 41 tasks ---> About Interfaces
+
+// Интерфейс — это конструкция языка TypeScript, которая используется, чтобы описывать объекты и функции.
